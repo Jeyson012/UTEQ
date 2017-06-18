98462d73660eaffac399ba3db354f0affe660550
Jeyson012 <jeysonpantoja@gmail.com>
2017-06-18 13:36:38 -0500

    promedio de 10 notas

---
+++ Promedio de 10 notas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class promedio_de_10_notas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INGRESE SUS NOTAS:");
+            int notas, i;
+            double acum, promedio;
+            acum = 0;
+            for (i = 1; i <= 10; i++)
+            {
+                notas = int.Parse(Console.ReadLine());
+                acum = acum + notas;
+            }
+
+                promedio = acum / 10;
+                Console.WriteLine("EL PROMEDIO ES: " + promedio);
+                Console.ReadKey();
+            
+        }
+    }
+}
