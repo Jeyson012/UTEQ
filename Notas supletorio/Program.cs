3a78de135dd343e4315ed2d7b14338433b80db67
Jeyson012 <jeysonpantoja@gmail.com>
2017-06-18 14:13:01 -0500

    programacón

---
+++ Notas supletorio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Estado_académico_de_estudiante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nt;
+            Console.WriteLine("Ingresa la nota del estudiante:");
+            nt = float.Parse(Console.ReadLine());
+
+            if (nt >= 7)
+            {
+                Console.Write("Estudiante Aprobado");
+
+            }
+
+            else
+            {
+                if (nt >= 3)
+                {
+                    Console.Write("Estudiante a Supletorio");
+
+                }
+                else
+                {
+                    Console.Write("Estudiante Reprobado");
+                }
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
